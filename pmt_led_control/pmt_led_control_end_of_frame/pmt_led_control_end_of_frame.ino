2f7ec1c253d237b644de05474a8529d0862177dd
dmossing <dan.mossing@gmail.com>
2017-01-20 10:14:22 -0800

    added arduino control code

---
+++ pmt_led_control/pmt_led_control_end_of_frame/pmt_led_control_end_of_frame.ino
@@ -0,0 +1,66 @@
+int pin_cam0 = 11;
+int pin_led = 12;
+int pin_pmt = 13;
+int pin_test = 10;
+int wait_time = 5;
+int waittrig = 9;
+int period = 60;
+static unsigned long t = 0;
+static unsigned long tlast = 0;
+static unsigned long ttrig = 0;
+
+boolean cam0 = false;
+boolean old_cam0 = false;
+boolean image2p = true;
+boolean keep_waiting = false;
+boolean posttrig = false;
+
+void setup()
+{
+  pinMode(pin_cam0, INPUT);
+  pinMode(pin_led, OUTPUT);
+  pinMode(pin_pmt, OUTPUT);
+  pinMode(pin_test, OUTPUT);
+}
+
+void loop()
+{
+  cam0 = digitalRead(pin_cam0);
+  t = millis();
+//  cam0 = ((t % period) < period/2);
+  digitalWrite(pin_cam0, cam0);
+  digitalWrite(pin_test, image2p);
+  if (cam0 && !old_cam0)
+  {
+    ttrig = t;
+    posttrig = true;
+  }
+  else if (posttrig && t - ttrig > waittrig)
+  {
+    tlast = t;
+    image2p = !image2p;
+    if (image2p)
+    {
+      digitalWrite(pin_led, LOW);
+    }
+    else
+    {
+      digitalWrite(pin_pmt, LOW);
+    }
+    keep_waiting = true;
+    posttrig = false;
+  }
+  else if (keep_waiting && t-tlast > wait_time)
+  {
+    if (image2p)
+    {
+      digitalWrite(pin_pmt, HIGH);
+    }
+    else
+    {
+      digitalWrite(pin_led, HIGH);
+    }
+    keep_waiting = false;
+  }
+  old_cam0 = cam0;
+  }
