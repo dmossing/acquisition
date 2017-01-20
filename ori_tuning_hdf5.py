31c4b65629af7fa545c31c0510d5bbf0fc23982c
dmossing <mossing@berkeley.edu>
2017-01-20 10:17:50 -0800

    added code for assigning stim times from stims.txt for LF

--- ori_tuning_hdf5.py
+++ ori_tuning_hdf5.py
@@ -6,6 +6,9 @@ import sys
 import glob
 import LFutils as ut
 import os
+import tkinter as tk
+from tkinter import filedialog
+import scipy.io as sio
 
 filenames = glob.glob('*.dat')
 filenames.sort()
@@ -16,6 +19,7 @@ N = len(filenames)
 tgt_fname = input('HDF5 filename: ')
 if not tgt_fname.endswith('.hdf5'):
     tgt_fname = tgt_fname+'.hdf5'
+print(tgt_fname)
 assert(not os.path.exists(tgt_fname))
 
 f = h5py.File(tgt_fname,'w')
@@ -26,12 +30,20 @@ g['frame_rate'] = float(input('frame rate (Hz): '))
 g['animalid'] = input('animal ID: ')
 g['stim_type'] = input('stim delivery: ')
 g['notes'] = input('notes: ')
+stim_frames = [int(line.strip()) for line in open('stims.txt')]
+g['stim_frames'] = np.asarray(stim_frames)
+
+print('select stim file')
+root = tk.Tk()
+root.withdraw()
+filepath = filedialog.askopenfilename()
+matfile = sio.loadmat(filepath,squeeze_me=True)
+result = matfile['result']
+g['stim_orientations'] = result['stimParams'][()][0]
 g.create_dataset('raw',(N,L1,L2),dtype=np.uint16)
 for j,name in enumerate(filenames):
     with open(name,'rb') as fid:
         frame = np.fromfile(fid,dtype=np.uint16).reshape((2048,2048))
     g['raw'][j] = frame
     print(j)
-stim_frames = [int(line.strip()) for line in open('stims.txt')]
-g['stim_frames'] = np.asarray(stim_frames)
 f.close()
