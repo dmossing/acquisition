3f0cfbe91e27b2dd129bc1bbd5b9ebf33ce19b23
dmossing <mossing@berkeley.edu>
2017-02-02 14:08:10 -0800

    fixing up integration of LF, 2P

--- gen2Pframes.m
+++ gen2Pframes.m
@@ -7,11 +7,13 @@ load([fname '.align'],'-mat','MCdata')
 Images = applyMotionCorrection(Images, MCdata);
 folder = strsplit(fname,{'/','\'},'CollapseDelimiters',true);
 folder = [strjoin(folder(1:end-1),'/') '/'];
+load([fname '.mat'],'info')
+dlmwrite([folder 'stims.txt'],info.frame-1,'\n');
 framefolder = [folder '2Pframes/'];
 mkdir(framefolder);
 L = size(Images,5);
 k = length(num2str(L-1));
 for t=1:L
     t
-    imwrite(squeeze(Images(:,:,1,1,t)),[framefolder ddigit(t,k) '.tif']);
+    imwrite(squeeze(Images(:,:,1,1,t)),[framefolder ddigit(t-1,k) '.tif']);
 end
--- ori_tuning_hdf5.py
+++ ori_tuning_hdf5.py
@@ -10,7 +10,7 @@ import tkinter as tk
 from tkinter import filedialog
 import scipy.io as sio
 
-filenames = glob.glob('*.dat')
+filenames = glob.glob('frames/*.dat')
 filenames.sort()
 
 N = len(filenames)
@@ -19,6 +19,12 @@ N = len(filenames)
 tgt_fname = input('HDF5 filename: ')
 if not tgt_fname.endswith('.hdf5'):
     tgt_fname = tgt_fname+'.hdf5'
+tgt_fold = os.path.dirname(tgt_fname)
+if tgt_fold:
+    tgt_fold = tgt_fold + '/'
+    if not os.path.isdir(tgt_fold):
+        os.mkdir(tgt_fold)
+    os.chdir(tgt_fold)
 print(tgt_fname)
 assert(not os.path.exists(tgt_fname))
 
@@ -31,7 +37,8 @@ g['animalid'] = input('animal ID: ')
 g['stim_type'] = input('stim delivery: ')
 g['notes'] = input('notes: ')
 stim_frames = [int(line.strip()) for line in open('stims.txt')]
-g['stim_frames'] = np.asarray(stim_frames)
+glf = g.create_group('LF')
+glf['stim_frames'] = np.asarray(stim_frames)
 
 print('select stim file')
 root = tk.Tk()
@@ -39,11 +46,17 @@ root.withdraw()
 filepath = filedialog.askopenfilename()
 matfile = sio.loadmat(filepath,squeeze_me=True)
 result = matfile['result']
-g['stim_orientations'] = result['stimParams'][()][0]
-g.create_dataset('raw',(N,L1,L2),dtype=np.uint16)
-for j,name in enumerate(filenames):
-    with open(name,'rb') as fid:
-        frame = np.fromfile(fid,dtype=np.uint16).reshape((2048,2048))
-    g['raw'][j] = frame
-    print(j)
+glf['stim_orientations'] = result['stimParams'][()][0]
+raw_fname = 'rawLF.hdf5'
+with h5py.File(tgt_fold+raw_fname,'w') as fraw:
+    for attr in ['date','animalid','notes']:
+        fraw[attr] = g[attr]
+    frames = fraw.create_dataset('frames',(N,L1,L2),dtype=np.uint16)
+    #glf.create_dataset('raw',(N,L1,L2),dtype=np.uint16)
+    for j,name in enumerate(filenames):
+        with open(name,'rb') as fid:
+            frame = np.fromfile(fid,dtype=np.uint16).reshape((2048,2048))
+        frames[j] = frame
+        print(j)
+glf['raw_frames'] = h5py.ExternalLink(raw_fname,tgt_fold)
 f.close()
