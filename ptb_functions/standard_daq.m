29d040a0329f3f81c768abf42a436261738ef740
dmossing <mossing@berkeley.edu>
2017-02-23 17:30:03 -0800

    added code to generate daq object automatically

---
+++ ptb_functions/standard_daq.m
@@ -0,0 +1,16 @@
+function d = standard_daq(n)
+% set up DAQ
+% up to 3 channels: first is stim trigger, second is complete stim protocol/move in z, third is projector LED on 
+
+if ~exists('n','var') || isempty(n)
+    n = 1;
+end
+
+d=daq.createSession('ni');
+addDigitalChannel(d,'Dev3','port0/line0','OutputOnly'); % stim trigger
+if n>1
+    addDigitalChannel(d,'Dev3','port0/line2','OutputOnly'); % complete stim protocol, move in z
+end
+if n>2
+    addDigitalChannel(d,'Dev3','port0/line1','OutputOnly'); % projector LED on
+end
