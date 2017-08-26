4ea47282a57e86817559421e165010b9dd8a3106
dmossing <mossing@berkeley.edu>
2017-08-25 19:05:06 -0700

    added code to run quick 2P retinotopy analysis. Updated getSignals to work with Evan's latest pipeline

--- getSignals.m
+++ getSignals.m
@@ -2,15 +2,17 @@ function getSignals(fname)
 i = 1;
 fns{i} = fname;
 ROIFile = [fns{i}, '.rois'];
-ROIdata = sbxDistribute(fns{i}, 'Save', 'SaveFile', ROIFile); % intialize struct
+ROIdata = sbxDistribute(fns{i}, 'Save'); % , 'SaveFile', ROIFile); % intialize struct
 createMasks(ROIdata, 'Save', 'SaveFile', ROIFile); % create ROI masks
 if ~strfind(fns{i},'depth')
     config = load2PConfig([fns{i}, '.sbx']);
-    [~, Data, Neuropil, ~] = extractSignals([fns{i},'.sbx'], ROIFile, 'all', 'Save', 'SaveFile', ROIFile, 'MotionCorrect', [fns{i},'.align'], 'Frames', 1:config.Frames-1);
+    [~, Data, Neuropil, ~] = extractSignals([fns{i},'.sbx'], ROIFile, 'all', 'Save', 'MotionCorrect', [fns{i},'.align'], 'Frames', 1:config.Frames-1); % 'SaveFile', ROIFile, 
 else
     strbase = strsplit(fns{i},'_depth');
+    Depth = str2num(strbase{2}(1));
     strbase = strbase{1};
     config = load2PConfig([strbase, '.sbx']);
-    [~, Data, Neuropil, ~] = extractSignals([strbase,'.sbx'], ROIFile, 'all', 'Save', 'SaveFile', ROIFile, 'MotionCorrect', [fns{i},'.align'], 'Frames', 1:config.Frames-1,'Depth',i);
+%     Frames = idDepth([strbase,'.sbx'],[],'Depth',1)';
+    [~, Data, Neuropil, ~] = extractSignals([strbase,'.sbx'], ROIFile, 'all', 'Save', 'MotionCorrect', [fns{i},'.align'], 'Depth',Depth); % 'SaveFile', ROIFile, % 'Frames', 1:config.Frames-1,
 end
 save(ROIFile, 'Data', 'Neuropil', '-mat', '-append');
