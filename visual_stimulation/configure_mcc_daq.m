b06902aee54a9fa72039ec6b9a2b6d7dc7d89407
dmossing <dan.mossing@gmail.com>
2018-09-11 10:19:24 -0700

    added code to configure MCC DAQ for visual stim

---
+++ visual_stimulation/configure_mcc_daq.m
@@ -0,0 +1,7 @@
+function d = configure_mcc_daq()
+
+d = DaqFind;
+err = DaqDConfigPort(d,0,0); % port A as input
+err = DaqDConfigPort(d,1,1); % port B as output
+
+DaqDOut(d,0,0); % clear existing high voltages
